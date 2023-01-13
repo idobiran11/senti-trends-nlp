790df53e8f524259c2ae86bcb455412c87228561
idobiran11 <ido.b@proteantecs.com>
2023-01-13 14:27:05 +0200

    basic class

--- Backend_Api.py
+++ Backend_Api.py
@@ -1,19 +1,26 @@
 import requests
 import datetime
 
-API_KEY = 'P5BOlzt1xIFW4USj5SRoN4jl5hYQAAyQ'
-SECRET_KEY = 'zkJPCUJiSwFWJ62x'
 
-# Set the search term
-search_term = "Donald Trump"
+class NewYorkTimesAPI:
+    API_KEY = 'P5BOlzt1xIFW4USj5SRoN4jl5hYQAAyQ'
+    SECRET_KEY = 'zkJPCUJiSwFWJ62x'
+    DATE_NOW = datetime.datetime.now()
 
-# Set the date range (past year)
-now = datetime.datetime.now()
-past_year = (now - datetime.timedelta(days=365)).strftime("%Y%m%d")
+    def __init__(self, search_term, search_time_backwards_days):
+        self.search_term = search_term
+        self.search_time_days = (self.DATE_NOW - datetime.timedelta(days=search_time_backwards_days)).strftime("%Y%m%d")
 
-# Make the request
-url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q={search_term}&begin_date={past_year}&api-key={api_key}"
-response = requests.get(url)
+    def make_request(self):
+        url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q={self.search_term}&begin_date={self.search_time_days}&api-key={self.API_KEY}"
+        response = requests.get(url)
+        data = response.json()
+        return data
 
-# Parse the JSON response
-data = response.json()
+
+if __name__ == '__main__':
+    search_term = input("Enter a search term: ")
+    search_time_backwards_days = int(input("Enter the number of days to search back: "))
+    ny_times_api = NewYorkTimesAPI(search_term, search_time_backwards_days)
+    data = ny_times_api.make_request()
+    print(data)
