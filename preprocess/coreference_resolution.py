3ec65fdab47a4f53645c5287cc961602f140c919
itay <itaynzohar@gmail.com>
2023-03-02 16:38:43 +0200

    use "cache"

--- preprocess/coreference_resolution.py
+++ preprocess/coreference_resolution.py
@@ -4,7 +4,7 @@ from spacy.tokens import Doc
 from utils.constants import PreprocessNames
 import os
 from utils.config_neptune import neptune_run
-
+from os.path import isfile
 DATA_DIR = "data"
 
 
@@ -15,22 +15,28 @@ def no_preprocess(news_vendor: str, object_name: str, input_csv_name: str,
 
 def coref_preprocess(news_vendor: str, object_name: str, input_csv_name: str,
                      output_dir: str = "data/preprocessed_data"):
-    nlp = spacy.load("en_coreference_web_trf")
+
     df = pd.read_csv(f'{DATA_DIR}/{input_csv_name}')
     output_filename = f'{news_vendor}_{object_name}_{PreprocessNames.COREF}.csv'
     full_output_path = f'{output_dir}/{output_filename}'
-    if not os.path.isfile(full_output_path):
+
+    if os.path.isfile(full_output_path):
+        df = pd.read_csv(full_output_path)
+    else:
+        nlp = spacy.load("en_coreference_web_trf")
         for index, row in df.iterrows():
             text = row['text']
             doc = nlp(text)
             print(doc.spans)
             coref_text = resolve_references(doc, object_name)
             df.at[index, 'text'] = coref_text
-    try:
-        df.to_csv(full_output_path)
-        neptune_run[news_vendor + "preprocessed_dataset"].upload(full_output_path)
-    except Exception as e:
-        df.to_csv(f'{output_filename}')
+        try:
+            df.to_csv(full_output_path)
+            neptune_run[news_vendor +
+                        "preprocessed_dataset"].upload(full_output_path)
+        except Exception as e:
+            df.to_csv(f'{output_filename}')
+
     return df
 
 
@@ -60,7 +66,8 @@ def resolve_references(doc: Doc, object: str = None) -> str:
             # Iterate through every other span in the cluster
             for mention_span in list(cluster):
                 # Set first_mention as value for the first token in mention_span in the token_mention_mapper
-                token_mention_mapper[mention_span[0].idx] = object + mention_span[0].whitespace_
+                token_mention_mapper[mention_span[0].idx] = object + \
+                    mention_span[0].whitespace_
 
                 for token in mention_span[1:]:
                     # Set empty string for all the other tokens in mention_span
